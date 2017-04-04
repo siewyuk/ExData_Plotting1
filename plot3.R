a6101cdfefc89ebcc5df876e0de84e8a40de9ac9
siewyuk <cpekwah@gmail.com>
2017-04-05 01:02:46 +0800

    second commit

--- plot3.R
+++ plot3.R
@@ -16,3 +16,12 @@ dat <- read.table("household_power_consumption.txt",
                                  "numeric",
                                  "numeric",
                                  "numeric"))
+
+#Subset data as only using data from the dates 2007-02-01 and 2007-02-02.
+data <- dat[dat$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#Format date, paste with time and perform POSIXct date-time conversion
+data$Date <- as.Date(data$Date, format = "%d/ %m/ %Y")
+datetime <- paste(as.Date(data$Date), data$Time)
+data$datetime <- as.POSIXct(datetime)
+
