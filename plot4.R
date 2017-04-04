b123ed5093d093b5198bc6893003da1093370d5f
siewyuk <cpekwah@gmail.com>
2017-04-05 01:08:56 +0800

    third commit

--- plot4.R
+++ plot4.R
@@ -36,4 +36,30 @@ data$datetime <- as.POSIXct(datetime)
 #Create partition for 2x2 plots
 par(mfrow = c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0))
 
+#Plot 1: x-axis = datetime, y-axis = Global_active_power
+plot(data$datetime, data$Global_active_power, type = "l", 
+     xlab = "", ylab = "Global Active Power (kilowatts)")
+
+#Plot 2: x-axis = datetime, y-axis = Voltage
+plot(data$datetime, data$Voltage, type = "l", 
+     xlab = "datetime", ylab = "Voltage (volt)")
+
+#Plot 3: x-axis = datetime, y-axis = Sub_metering 1, 2 & 3.
+plot(data$datetime, data$Sub_metering_1, type = "l", 
+     xlab = "", ylab = "Global Active Power (kilowatts)")
+lines(data$datetime, data$Sub_metering_2, col = "red")
+lines(data$datetime, data$Sub_metering_3, col = "blue")
+
+#Add legend on topright
+legend("topright", col = c("black", "red", "blue"),
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       lty = 1, bty = "n")
+
+#Plot 4: x-axis = datetime, y-axis = Global_reactive_power
+plot(data$datetime, data$Global_reactive_power, type = "l", 
+     xlab= "datetime", ylab = "Global Rective Power (kilowatts)")
+
+#Save to plot according to specified width and height
+dev.copy(png, file = "plot4.png", width = 480, height = 480)
+dev.off()
 
