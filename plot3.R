f1ba3705d6bc26a6b15846d1e3d457d5c3e536c5
siewyuk <cpekwah@gmail.com>
2017-04-05 01:03:21 +0800

    third commit

--- plot3.R
+++ plot3.R
@@ -25,3 +25,22 @@ data$Date <- as.Date(data$Date, format = "%d/ %m/ %Y")
 datetime <- paste(as.Date(data$Date), data$Time)
 data$datetime <- as.POSIXct(datetime)
 
+#Create plot: x-axis = Sub_metering_1 & y-axis = Global_active_power. Line color = black
+plot(data$datetime, data$Sub_metering_1, type = "l", 
+     xlab = "", ylab = "Global Active Power (kilowatts)")
+
+#Add second plot: x-axis = Sub_metering_2. Line color = red 
+lines(data$datetime, data$Sub_metering_2, col = "red")
+
+#Add third plot: x-axis = Sub_metering_3. Line color = blue
+lines(data$datetime, data$Sub_metering_3, col='blue')
+
+#Add legend description on top right corner
+legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2, 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+#Save to plot according to specified width and height
+dev.copy(png, file="plot3.png", width = 480, height = 480)
+dev.off()
+
+
