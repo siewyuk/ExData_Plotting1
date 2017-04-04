9b550d1115f3003ed5fa2f5d12efede1217769e7
siewyuk <cpekwah@gmail.com>
2017-04-05 00:40:00 +0800

    second commit

--- plot1.R
+++ plot1.R
@@ -16,4 +16,14 @@ dat <- read.table("~/Downloads/household_power_consumption.txt",
                                  "numeric",
                                  "numeric"))
 
+#Subset data as only using data from the dates 2007-02-01 and 2007-02-02.
+data <- dat[dat$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#Create histogram: x-axis = Global active power & y-axis = Frequency.
+hist(data$Global_active_power, col = "red", main = "Global Active Power",
+     xlab = "Global Active Power (killowatts)", ylab = "Frequency")
+
+#Save to histogram according to specified width and height
+dev.copy(png, file = "plot1.png", width = 480, height = 480)
+
 
