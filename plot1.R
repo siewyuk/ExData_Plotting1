96f11eadb7fb76814721569ca7530907f09a9685
siewyuk <cpekwah@gmail.com>
2017-04-05 00:39:26 +0800

    first commit

--- plot1.R
+++ plot1.R
@@ -1,1 +1,19 @@
+#plot1.R
+#Read in txt data. Use colClasses to characters and numerics.
+#Loading will take some time for all 2,075,259 rows of data.
+dat <- read.table("~/Downloads/household_power_consumption.txt", 
+                  sep = ";", 
+                  header = TRUE, 
+                  na.strings = "?", 
+                  stringsAsFactor = FALSE, 
+                  colClasses = c("character",
+                                 "character",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric"))
+
 
